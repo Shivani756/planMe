95fee7229f7310441770274733497d27f77fa670
Shivani <139329176+Shivani756@users.noreply.github.com>
2024-06-11 01:34:09 +0530

    first commit
    This is my first commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+        Organizing an event, whether corporate, special, or trade events, is a challenging task. However meticulous and enthusiastic you are, you might be weary of planning and organizing events if it isn’t your usual task. The worst is that you might neglect other essential details of the events that won’t meet quality expectations. That’s where the event planner comes like a superhero! Event planners exist to organize events well ahead of time. From facilities, locations, special guests, equipment, catering, and other big and small details, event planners ensure that all are taken care of. This way, businesses, couples, and celebrants will have time to relax and focus on the most important matters.
+
+Today, various event planners provide incredible opportunities for brands and couples to create an ultimate lasting impression on the audience. Fortunately, you can also find them online. If you’re one of them but do not have the ideal website yet, this list is a perfect inspiration set. These event planner websites will inspire you to build your online visibility with sophistication.
+      </p>
+    </section>
+  );
+};
+
+export default About;
